595cf72bd739e14cd484190de8d71a541bf6abf5
wingie125 <arbab.vns123@gmail.com>
2025-03-22 02:45:21 +0530

    small fix

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -16,7 +16,7 @@ export const HomeNavbar = () => {
           <Link prefetch href="/" className="hidden md:block">
             <div className="p-4 flex items-center gap-1">
               <Image src="/logo.svg" alt="logo" width={32} height={32} />
-              <p className="text-xl font-semibold tracking-tight">InTube</p>
+              <p className="text-xl font-semibold tracking-tight">newTube</p>
             </div>
           </Link>
         </div>
